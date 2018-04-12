a2470eeb9cb8bdb31363ec952938eeda853dcc78
Jason Lin <jason.lin.sjtu@gmail.com>
2018-04-12 07:48:04 +0000

    building prototype politics simulation

--- psychsim/afrobarometer/__main__.py
+++ psychsim/afrobarometer/__main__.py
@@ -5,6 +5,8 @@ from psychsim.action import *
 from psychsim.world import *
 from psychsim.agent import *
 
+import configure
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 ACTION_PATH = os.path.join(dir_path, 'action_list.txt')
 AGENT_PATH = os.path.join(dir_path, 'agent_list.txt')
@@ -16,35 +18,53 @@ class AfroWorldInitializer:
         self.world = world
         
         self.init_agent()
-        self.init_action()
         self.init_state()
-        # import pdb; pdb.set_trace()
+        self.init_action()
+        self.set_reward()
+        
         # breakpoint to see any changes in world
     
     def init_agent(self):
-        agent_names = [line.rstrip('\n') for line in open(AGENT_PATH)]
-        for agent_name in agent_names:
-            agent = Agent(agent_name)
-            agent.setHorizon(1)
-            self.world.addAgent(agent)
+        r1 = Agent('resident1')
+        r1.setHorizon(1)
+        self.world.addAgent(r1)
+        r2 = Agent('resident2')
+        r2.setHorizon(1)
+        self.world.addAgent(r2)
     
     def init_action(self):
-        action_names = [line.rstrip('\n') for line in open(ACTION_PATH)]
         # bind actions to agents
-        for agent in self.world.agent.values():
-            for action_name in action_names:
-                agent.addAction({'verb': action_name})
+        agent = self.world.agents['resident1']
+        for action_name, value in configure.actions_trust.items():
+            for state in value:
+                action = agent.addAction({'verb': action_name})
+                prob_increase = value[state]
+                prob_decrease = 1. - prob_increase
+                tree = makeTree({'distribution':[
+                    (incrementMatrix(stateKey(agent.name, state), 10),
+                     prob_increase),
+                    (incrementMatrix(stateKey(agent.name, state), 10),
+                     prob_decrease)
+                    ]})
+                self.world.setDynamics(stateKey(agent.name, state), action, tree)
         
         # set dynamics
     
     def init_state(self):
-        state_names = [line.rstrip('\n') for line in open(STATE_PATH)]
-        for agent in self.world.agent.values():
-            for state_name in state_names:
+        for agent in self.world.agents.values():
+            for state_name in configure.states:
                 # set default to 100 
                 # (TODO: change the default number)
-                agent.setState(state_name, 100)
+                self.world.defineState(agent.name, state_name, int)
+                self.world.setState(agent.name, state_name, 50)
+                
+    def set_reward(self):
+        for agent in self.world.agents.values():
+            agent.setReward(maximizeFeature('economy'))
 
 if __name__ == '__main__':
     world = World()
     awi = AfroWorldInitializer(world)
+    world.setOrder(world.agents.keys())
+    result = world.step()
+    print(result)
