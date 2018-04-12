cedb22c1347358e128e588177d2657ed38cd2981
Jason Lin <jason.lin.sjtu@gmail.com>
2018-04-12 07:48:32 +0000

    using a module to save constant vars

---
+++ psychsim/afrobarometer/configure.py
@@ -0,0 +1,35 @@
+actions_trust = {
+    'refuse tax': \
+        {'economy': 0.7,
+         'living_condition': 0.3,
+         'safety': 0.3,
+        },
+    'vote': \
+        {'economy': 0.7,
+         'education': 0.7,
+         'living_condition': 0.7,
+         'safety': 0.7,
+        },
+    'demonstration': \
+        {'economy': 0.6,
+         'education': 0.6,
+         'living_condition': 0.3,
+         'safety': 0.3,
+        },
+    'raise_issue': \
+        {'economy': 0.6,
+         'education': 0.7,
+         'living_condition': 0.7,
+         'safety': 0.7,
+        },
+}
+
+beliefs = {
+    
+}
+
+objectives = {
+    
+}
+
+states = ['economy', 'education', 'living_condition', 'safety']
