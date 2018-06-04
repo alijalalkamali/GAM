261f250160c54db007962c7ab8ffc3e382419be3
Jason Lin <jason.lin.sjtu@gmail.com>
2018-06-04 18:06:25 +0000

    Remove deprecated configurations

--- psychsim/afrobarometer/configure.py
+++ psychsim/afrobarometer/configure.py
@@ -1,32 +1,6 @@
 lookup_states = {296: {'name':'close2party', 'type':int}}
 preset_states = {'economy': {'type':int, 'val':50}}
 
-actions_trust = {
-    'refuse tax': \
-        {'economy': 0.7,
-         'living_condition': 0.3,
-         'safety': 0.3,
-        },
-    'vote': \
-        {'economy': 0.6,
-         'education': 0.7,
-         'living_condition': 0.7,
-         'safety': 0.7,
-        },
-    'demonstration': \
-        {'economy': 0.6,
-         'education': 0.6,
-         'living_condition': 0.3,
-         'safety': 0.3,
-        },
-    'raise_issue': \
-        {'economy': 0.6,
-         'education': 0.7,
-         'living_condition': 0.7,
-         'safety': 0.7,
-        },
-}
-
 beliefs = {
     
 }
